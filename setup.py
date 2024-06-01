deea15ef6bd68f4a46262d9ec93aa6cb4001ad74
SKoprek <szymon.d.koprek@gmail.com>
2024-06-01 03:35:51 +0200

    README.MD

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
+import pathlib
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README_PIPY.md').read()
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='dev_fancy_console',
-    version='1.0',
+    version='0.0.1',
     description='Stylized Python console, a project made for fun',
     author="SKoprek",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     url='https://github.com/SKoprek/python-fancy-console',
     entry_points={
