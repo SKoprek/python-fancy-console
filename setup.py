e2894113c763c39b92e035aec4c6d4e1bcf72132
SKoprek <szymon.d.koprek@gmail.com>
2024-06-01 04:47:32 +0200

    UPDATE

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 import pathlib
 
-here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / 'README.rst').read_text(encoding='utf-8')
+def read(*pathcomponents):
+    """Read the contents of a file located relative to setup.py"""
+    with open(join(abspath(dirname(__file__)), *pathcomponents)) as thefile:
+        return thefile.read()
 
 setup(
     name='dev_fancy_console',
     version='0.0.1',
     description='Stylized Python console, a project made for fun',
     author="SKoprek",
-    long_description=readme,
-    long_description_content_type='text/rst',
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
     packages=find_packages(),
     url='https://github.com/SKoprek/python-fancy-console',
     entry_points={
