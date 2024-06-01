c584758aa4f35c76905389c52b44f6c7f2a3c1be
SKoprek <szymon.d.koprek@gmail.com>
2024-06-01 02:22:47 +0200

    Build v0.1

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
 setup(
     name='fancy_console',
     version='0.1',
+    description='Stylized Python console, a project made for fun.',
+    author="SKoprek",
+    long_description=readme(),
     packages=find_packages(),
+    url='https://github.com/SKoprek/python-fancy-console',
     entry_points={
         "console_scripts": [
             "fancy_example_1 = fancy_console.examples:example_1",
